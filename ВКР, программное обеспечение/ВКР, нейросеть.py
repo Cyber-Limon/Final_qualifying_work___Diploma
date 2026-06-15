81b64af786d9f7d17b7697c59afa2f1b922d7efc
Max <162749285+Cyber-Limon@users.noreply.github.com>
2026-06-15 12:31:06 +0500

    Add files via upload

--- ВКР, программное обеспечение/ВКР, нейросеть.py
+++ ВКР, программное обеспечение/ВКР, нейросеть.py
@@ -6,23 +6,31 @@ import pandas as pd
 from tensorflow                  import keras
 from sklearn.preprocessing       import StandardScaler
 from sklearn.model_selection     import train_test_split
-from tensorflow.keras.layers     import Dense, Dropout, BatchNormalization
+from tensorflow.keras.layers     import Dense, Dropout
 from tensorflow.keras.callbacks  import EarlyStopping
 from tensorflow.keras.optimizers import Adam
 
 
 
-def augmentation(X, y, quantity):
-    X_aug = list(X.copy())
-    y_aug = list(y.copy())
+def augmentation(X, y, quantity, bias):
+    X_current = []
+    y_current = []
+
+    for material in range(len(X)):
+        if y[material][material_property] != 0:
+            X_current.append(X[material])
+            y_current.append(y[material][material_property])
+
+    X_aug = list(X_current.copy())
+    y_aug = list(y_current.copy())
 
     while len(X_aug) < quantity:
         material  = random.randint(0, len(X) - 1)
-        X_current = X[material].copy()
+        x_current = X[material].copy()
 
         sample = []
-        for i in range(len(X_current)):
-            if X_current[i] > 0.01:
+        for i in range(len(x_current)):
+            if x_current[i] > bias:
                 sample.append(i)
 
         if len(sample) >= 6:
@@ -37,12 +45,12 @@ def augmentation(X, y, quantity):
         changes = random.sample(sample, number_of_changes)
         for change in range(len(changes)):
             if change % 2 == 0:
-                X_current[changes[change]] += 1
+                x_current[changes[change]] += bias
             else:
-                X_current[changes[change]] -= 1
+                x_current[changes[change]] -= bias
 
-        X_aug.append(X_current)
-        y_aug.append(y[material])
+        X_aug.append(x_current)
+        y_aug.append(y_current[material])
 
     return X_aug, y_aug
 
@@ -64,6 +72,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=1107,
     shuffle     =True)
 
+
+
 scaler_X = StandardScaler()
 X_train  = scaler_X.fit_transform(X_train)
 X_test   = scaler_X.transform(X_test)
@@ -72,32 +82,14 @@ scaler_Y = StandardScaler()
 y_train  = scaler_Y.fit_transform(y_train)
 y_test   = scaler_Y.transform(y_test)
 
-y_train = y_train.T
-y_test  = y_test.T
-
 joblib.dump(scaler_X, "ВКР, нормализаторы\\scaler_X.pkl")
 joblib.dump(scaler_Y, "ВКР, нормализаторы\\scaler_Y.pkl")
 
 
 
 for material_property in range(len(properties[0])):
-    X_train_current = []
-    X_test_current  = []
-    y_train_current = []
-    y_test_current  = []
-
-    for material in range(len(X_train)):
-        if y_train[material_property][material] != 0:
-            X_train_current.append(X_train[material])
-            y_train_current.append(y_train[material_property][material])
-
-    for material in range(len(X_test)):
-        if y_test[material_property][material] != 0:
-            X_test_current.append(X_test[material])
-            y_test_current.append(y_test[material_property][material])
-
-    X_train_current, y_train_current = augmentation(X_train_current, y_train_current, 2000)
-    X_test_current, y_test_current   = augmentation(X_test_current, y_test_current, 500)
+    X_train_current, y_train_current = augmentation(X_train, y_train, 2000, 0.01)
+    X_test_current,  y_test_current  = augmentation(X_test,  y_test,   500, 0.01)
 
     X_train_current = np.array(X_train_current)
     X_test_current  = np.array(X_test_current)
